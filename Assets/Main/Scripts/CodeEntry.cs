8a393f3a4c857223da8ceff28a27c3c0f5220441
Paul Provan <provanpm@mail.uc.edu>
2022-09-21 15:04:11 -0400

    Version control test / removed tutorial assets
    Small Chore Creation scene change to test version control
    
    - Removed tutorial assets

--- Assets/Main/Scripts/CodeEntry.cs
+++ Assets/Main/Scripts/CodeEntry.cs
@@ -84,7 +84,7 @@ public class CodeEntry : MonoBehaviour
                 }
                 else
                 {
-                    feedbackText.text = "Group Not Found";
+                    feedbackText.text = "Group not found";
                 }
             });
         }
